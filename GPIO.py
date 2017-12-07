dd4fe50fc1b76d24871603621413c8f4350a288b
Peradat Permjai <gullwings@msn.com>
2017-12-07 17:43:31 +0700

    start

---
+++ GPIO.py
@@ -0,0 +1,193 @@
+import RPi.GPIO as GPIO ## Import GPIO library
+import ConfigParser
+import os
+import time
+import requests
+import sys
+import socket
+import serial
+import fcntl
+import struct
+
+REMOTE_SERVER = "www.google.com"
+
+
+def internet_on():
+    try:
+        # see if we can resolve the host name -- tells us if there is
+        # a DNS listening
+        host = socket.gethostbyname(REMOTE_SERVER)
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        s = socket.create_connection((host, 80), 2)
+        return True
+    except:
+        pass
+    return False
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+def SendAlartFun(channel):
+    try:
+        resp = requests.get(nti_url+'car_id={0}'.format(id), timeout=2.001)
+        print ('content     ' + resp.content) 
+    except:
+        print 'SendAlartFun Connection lost'
+
+def SendStatusFun(message):
+    try:
+        ser.flushInput()
+        ser.flushOutput()
+        time.sleep(0.1)
+        ser.write('AT+QCCID\r')
+        time.sleep(2)
+
+        for num in range(0, 10):
+            bufsid = ser.readline()
+            if len(bufsid) >= 20 :
+                break
+
+        replacements = (',', '\r', '\n', '?')
+        for r in replacements:
+            bufsid = bufsid.replace(r, ' ')
+        SID = bufsid.split(" ")
+        print SID
+        #SID = CID[1]
+        
+        ser.write('AT+GSN\r')
+        time.sleep(1)
+        for num in range(0, 10):
+            bufemi = ser.readline()
+            if len(bufemi) >= 10 :
+                break
+
+        replacements = (',', '\r', '\n', '?')
+        for r in replacements:
+            bufemi = bufemi.replace(r, ' ')
+        IMEI = bufemi.split(" ")
+        print IMEI
+        #IMEI = emi[0]
+        sidOk = True
+        
+        print id
+        ip = get_ip_address('ppp0') 
+        print ip
+        print SID
+        print IMEI
+        api = nti_url.split("/")
+        print api[2]
+
+        if len(SID[1]) <= 13:
+            return False
+
+        resp = requests.get('http://188.166.197.107:8001?id={0}&ip={1}&sid={2}&imei={3}&api={4}&msg={5}'.format(id,ip,SID[1],IMEI[0],api[2],message), timeout=3.001)
+        print ('content     ' + resp.content) 
+        return True
+    except:
+        print 'SendStatusFun Connection lost'
+    return False
+
+
+        
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+if os.path.exists("/home/pi/usb/config.ini") == False:
+    print("config.ini error")
+    os.system('sudo mount /dev/sda1 /home/pi/usb/')
+    exit()
+
+
+#if os.path.exists("/home/pi/usb/pic") == True:
+#    print("Delete Pic Foder!!!")
+    #os.system('sudo mount /dev/sda1 /mnt/usbdrive')
+#    os.system('sudo rm -rf /home/pi/usb/pic')
+#    os.system('sudo mkdir /home/pi/usb/pic')
+ #   os.system('sudo mkdir /home/pi/usb/pic/ch0')
+#    os.system('sudo mkdir /home/pi/usb/pic/ch1')
+
+#if os.path.exists("/home/pi/usb/vdo") == True:
+ #   print("Delete Vdo Foder!!!")
+    #os.system('sudo mount /dev/sda1 /mnt/usbdrive')
+ #  os.system('sudo rm -rf /home/pi/usb/vdo')
+  #  os.system('sudo mkdir /home/pi/usb/vdo')
+ #   os.system('sudo mkdir /home/pi/usb/vdo/ch0')
+  #  os.system('sudo mkdir /home/pi/usb/vdo/ch1')
+    
+       
+Config = ConfigParser.ConfigParser()
+Config.read('/home/pi/usb/config.ini')
+
+id =  ConfigSectionMap('Profile')['id']
+timevdo = ConfigSectionMap('Profile')['timevdo']
+timepic = ConfigSectionMap('Profile')['timepic']
+nti_url = ConfigSectionMap('Profile')['nti_api']
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM) ## Use board pin numbering
+GPIO.setup(3, GPIO.IN) # Alaet
+GPIO.setup(4, GPIO.IN) # Power
+
+GPIO.add_event_detect(3, GPIO.RISING, callback=SendAlartFun, bouncetime=100)
+
+sendStart = False
+
+time.sleep(5)
+ser = serial.Serial('/dev/ttyUSB2', 115200, timeout=.5)
+ser.write('AT+QGPS=1\r')
+ser.write('ATE0\r')
+time.sleep(2)
+
+current_time = time.time()
+startTime = time.time()
+timeStart = time.time()
+timeout = time.time() + 60
+while True:
+
+    current_time = time.time()
+#I/O Power on
+    if sendStart == False  :
+        if internet_on() == True :  
+            if(SendStatusFun('Power Start') == True):
+                sendStart = True
+                print sendStart
+
+#I/O Power off
+    if(GPIO.input(4) == 0):
+        print('Power Off')
+        SendStatusFun('Power Off {0:.1f} Min'.format((current_time - timeStart)/60))
+        time.sleep(10)
+        
+        os.system('sudo shutdown -h now')
+        break
+
+#On line status
+    if current_time - startTime > 60*10:
+        SendStatusFun('On {0:.1f} Min'.format((current_time - timeStart)/60))
+        startTime = current_time
+
+#Ennable GPS
+    if time.time() > timeout:
+        timeout = time.time() + 60
+        ser.write('ATE0\r')
+        ser.write('AT+QGPS=1\r')
+        for num in range(0, 5):
+            bufemi = ser.readline()
+
+GPIO.cleanup()
---
+++ GPS.py
@@ -0,0 +1,204 @@
+#! /usr/bin/python
+# Written by Dan Mandle http://dan.mandle.me September 2012
+# License: GPL 2.0
+ 
+import os
+import requests
+from gps import *
+from time import *
+import time
+import threading
+import RPi.GPIO as GPIO ## Import GPIO library
+import serial
+import time
+import ConfigParser
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+if os.path.exists("/home/pi/usb/config.ini") == False:
+    print("config.ini error")
+    os.system('sudo mount /dev/sda1 /home/pi/usb/')
+    exit()
+    
+Config = ConfigParser.ConfigParser()
+Config.read('/home/pi/usb/config.ini')
+
+id =  ConfigSectionMap('Profile')['id']
+timevdo = ConfigSectionMap('Profile')['timevdo']
+timepic = ConfigSectionMap('Profile')['timepic']
+gps_url = ConfigSectionMap('Profile')['gps_api']
+pic_url = ConfigSectionMap('Profile')['pic_api']
+
+gpsd = None #seting the global variable
+
+#checking port
+#port = 'Error'
+portOk = '/dev/ttyUSB1'
+#input2 = 0
+#for num in range(0, 4):
+#    port = '/dev/ttyUSB{0}'.format(num)
+#    try:
+#        ser = serial.Serial(port, 115200, timeout=.5)
+#        input = ser.inWaiting()
+#        print('Checking {0} Data In {1}'.format(port,input))
+#        time.sleep(5)
+#        input = ser.inWaiting()
+#    except:
+#        print('Port {0} busy'.format(port))
+
+#    try:
+#        input2 = 0
+#        for count in range(0, 10):
+#            inputS = ser.readline()
+#            input2 += len(inputS)
+#            print(inputS)
+#        ser.close()
+#    except:
+#        print('Port {0} Except'.format(port))
+#    print('Checked {0} Data In {1} DataS {2}'.format(port,input,input2))
+#    if(input > 200) or (input2 > 100):
+#        print('{0} Ok!!!'.format(port))
+#        portOk = port
+#        break
+#time.sleep(10)
+#os.system('clear') #clear the terminal (optional)
+
+os.system('sudo chmod +x /home/pi/pihos/connect.sh')
+
+os.system('sudo systemctl stop gpsd.socket')
+os.system('sudo systemctl disable gpsd.socket')
+os.system('sudo gpsd {0} -F /var/run/gpsd.sock'.format(portOk))
+os.system('sudo systemctl enable gpsd.socket')
+os.system('sudo systemctl start gpsd.socket')
+
+class GpsPoller(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    global gpsd #bring it in scope
+    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+    self.current_value = None
+    self.running = True #setting the thread running to true
+ 
+  def run(self):
+    global gpsd
+    while gpsp.running:
+      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+ 
+if __name__ == '__main__':
+  gpsp = GpsPoller() # create the thread
+  try:
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    #GPIO.setup(27, GPIO.OUT)#3G
+    GPIO.setup(22, GPIO.OUT)#GPS
+    
+    gpsp.start() # start it up
+    countSend = 0
+    countError = 0
+    timeout = time.time() + 30
+    while True:
+      #It may take a second or two to get good data
+      #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
+      
+      #os.system('clear')
+      #print
+      print 'GPS sending Seccess ' , countSend ,' Error ', countError  
+      #print '----------------------------------------'
+      #print 'latitude    ' , gpsd.fix.latitude
+      #print 'longitude   ' , gpsd.fix.longitude
+      #print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
+      #print 'Heading     ' , gpsd.fix.track,'deg (true)'
+      #print  gps_url,'?ambulance_id={0}&tracking_latitude={1:.6f}&tracking_longitude={2:.6f}&tracking_speed={3:.2f}'.format(id,gpsd.fix.latitude,gpsd.fix.longitude,gpsd.fix.speed)
+            
+      print  gps_url,'?ambulance_id={0}&tracking_latitude={1:.6f}&tracking_longitude={2:.6f}&tracking_speed={3:.2f}&tracking_heading={4}'.format(id,gpsd.fix.latitude,gpsd.fix.longitude,gpsd.fix.speed,gpsd.fix.track)
+      
+      
+      if str(gpsd.fix.latitude) != 'nan' and str(gpsd.fix.latitude) != '0.0':
+        GPIO.output(22,True)
+        try:
+          resp = requests.get(gps_url+'track_latitude={0:.6f}&track_longitude={1:.6f}&track_speed={2:.2f}&car_id={3}&tracking_heading={4}'.format(gpsd.fix.latitude,gpsd.fix.longitude,gpsd.fix.speed,id,gpsd.fix.track), timeout=1.001)
+          #resp = requests.get(gps_url+'?ambulance_id={0}&tracking_latitude={1:.6f}&tracking_longitude={2:.6f}&tracking_speed={3:.2f}&tracking_heading={4}'.format(id,gpsd.fix.latitude,gpsd.fix.longitude,gpsd.fix.speed,gpsd.fix.track), timeout=2.001)
+          #print 'status_code ' , resp.status_code
+          #print 'headers     ' , resp.headers
+          print 'content     ' , resp.content
+          GPIO.output(27,True)
+          if(resp.status_code == 200 ):
+            countSend+=1
+            countError = 0
+            timeout = time.time() + 30 #timeout reset
+          else:
+            countError+=1
+        except:
+          print 'ConnectionError'
+          time.sleep(1)
+          countError += 1
+          if countError > 20:
+            GPIO.output(27,False)
+            break
+          #continue
+		
+      GPIO.output(22,False)
+      time.sleep(0.95) #set to whatever
+      
+      if time.time() > timeout:
+        print "Timeout"
+        for count in range(0, 3):
+          time.sleep(0.2)
+          GPIO.output(22,True)
+          time.sleep(0.2)
+          GPIO.output(22,False)
+        break
+      if countError > 20:
+        print "countError"
+        for count in range(0, 6):
+          time.sleep(0.1)
+          GPIO.output(22,True)
+          time.sleep(0.1)
+          GPIO.output(22,False)
+        #GPIO.output(27,False)
+        break
+
+	  #print 'altitude (m)' , gpsd.fix.altitude
+      #print 'eps         ' , gpsd.fix.eps
+      #print 'epx         ' , gpsd.fix.epx
+      #print 'epv         ' , gpsd.fix.epv
+      #print 'ept         ' , gpsd.fix.ept
+      #print 'speed (m/s) ' , gpsd.fix.speed
+      #print 'climb       ' , gpsd.fix.climb
+      #print 'track       ' , gpsd.fix.track
+      #print 'mode        ' , gpsd.fix.mode
+      #print
+      #print 'sats        ' , gpsd.satellites
+      
+	  
+	  #r = urllib.request.urlopen('https://api.github.com', auth=('user', 'pass'))
+	  #var r = requests.get('http://www.google.com')
+	  
+      
+ 
+  except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+    print "\nKilling Thread..."
+    gpsp.running = False
+    gpsp.join() # wait for the thread to finish what it's doing
+    #GPIO.output(27,False)
+    GPIO.output(22,False)
+    GPIO.cleanup()
+    exit()
+
+  print "Done.\nExiting."
+  gpsp.running = False
+  gpsp.join() # wait for the thread to finish what it's doing
+  #GPIO.output(27,False)
+  GPIO.output(22,False)
+  GPIO.cleanup()
+  exit()
